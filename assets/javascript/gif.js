ca60fd205dd50b32f6e5318aef25e4d30de6fd6c
Scottie <avscott@pacbell.net>
2019-09-04 01:18:59 -0700

    add on click functions and styling

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,71 +1,111 @@
 //Declare variables
 //list of comedians
 var topics = ["Adam Sandler", "Amy Schumar", "Bernie Mac", "Bill Maher", "Carol Burnett", "Carrot Top", "Cedric The Entertainer", 
-"Chevy Chase", "Chris Farley", "Chris Tucker", "Damon Wayans", "Dick Gregory", "Eddie Murphy", "Ellen DeGeneres", "George Carlin", 
-"Gilda Radner", "Howie Mandel", "Jerry Seinfeld", "Jim Carrey", "John Belushi", "John Leguizamo", "Lucille Ball", "Kevin Hart", 
-"Martin Lawrence", "Mike Epps", "Moms Mabley", "Richard Pryor", "Robin Williams", "Steve Harvey", "Whoopie Goldberg"];
+"Chevy Chase", "Chris Farley", "Chris Tucker", "Damon Wayans", "Eddie Murphy", "Ellen DeGeneres", "George Carlin", "Gilda Radner", 
+"Howie Mandel", "Jerry Seinfeld", "Jim Carrey", "John Belushi", "John Leguizamo", "Leslie Jones", "Lucille Ball", "Kevin Hart", 
+"Martin Lawrence", "Mike Epps", "Richard Pryor", "Robin Williams", "Sarah Silvermann", "Steve Harvey", "Steve Martin", "Whoopie Goldberg"];
 
 //api key
 var key = "GYHcoSDnNTboO0ZRaXgPLt7ixB2VEDWn";
 
-//function to create buttons
-function buttons(comic) {
-    var name = comic.split(' ');
-    var dataName = name.join('+');
-    var button = $('<button>').text(comic);
-    button.addClass('btn btn-info m-2 comic');
-    button.attr('data-name', dataName);
-    $('#buttons').append(button);
-}
-
 //display buttons on screen
 topics.forEach(buttons);
 
-  //add click event to the button
-  $(".comic").on("click", function() {
-
-    //variable to hold api query
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-name") + "&api_key=" + key + "&limit=10";
-
-    //pulling data fron the api
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-
-      //after we get data from the api
-      .then(function(response) {
-          var images = response.data;
-          console.log(images);
-
-          //empty screen of any existing giphs
-          $("#giphs").empty();
-
-          for (i=0; i<images.length; i++) {
-              
-              //get image url from the api response
-              var imageUrl = images[i].embed_url;
-
-              //create div for rating
-              var rating = $("<div>");
-              rating.text(images[i].rating);
-
-              //creating wrapper div
-              var wrapper = $("<div>");
-              wrapper.addClass("gif-wrapper mr-5 mb-5");
-              wrapper.append(rating);
-              
-              //creating an image tag
-              var comicImage = $("<img>");
-
-              //adding image url from api
-              comicImage.attr("src", imageUrl);
-              comicImage.addClass("comic-image");
-              comicImage.attr("alt", $(this)[0].innerText);
-              wrapper.prepend(comicImage);
-
-              //dispaying image on screen
-              $("#giphs").append(wrapper);
-          }
-      });
+//function to create buttons
+function buttons(comic) {
+  var dataName = comic;
+  var name = comic.split(' ');
+  var dataSearch = name.join('+');
+  var button = $('<button>').text(comic);
+  button.addClass('btn btn-info m-2 comic');
+  button.attr({
+    'data-name': dataName,
+    'data-search': dataSearch
   });
+  $('#buttons').append(button);
+}
+
+//add click event to the buttons
+$(".comic").on("click", function() {
+  var alt = $(this).attr("data-name");
+
+  //variable to hold api query
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-search") + "&api_key=" + key + "&limit=10";
+
+  //pulling data fron the api
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+    //after we get data from the api
+    .then(function(response) {
+        var images = response.data;
+        console.log(images);
+
+        //empty screen of any existing giphs
+        $("#giphs").empty();
+
+        for (i=0; i<images.length; i++) {
+            
+            //get image urls from the api response
+            var imageUrl = images[i].images.original.url;
+            var stillUrl = images[i].images.original_still.url;
+
+            //create div for rating
+            var rating = $("<div>");
+
+            //check if rating exists
+            if (images[i].rating) {
+              //if rating exists
+              rating.text(images[i].rating);
+            } else {
+              //if no rating exists
+              rating.text("No Rating");
+            }
+
+            //creating wrapper div
+            var wrapper = $("<div>");
+
+            //add class info to wrapper for styling
+            wrapper.addClass("gif-wrapper mr-5 mb-5");
+            
+            //creating an image tag
+            var comicImage = $("<img>");
+
+            //adding image urls from api, still, animate and state attributes
+            comicImage.attr({
+              src: stillUrl,
+              "data-still": stillUrl,
+              "data-animate": imageUrl,
+              "data-state": "still",
+              alt: alt
+            });
+
+            //add comic-image class to image
+            comicImage.addClass("comic-image");
+
+            //add image and rating to wrapper
+            wrapper.append(comicImage, rating);
+
+            //dispaying image and rating on screen
+            $("#giphs").append(wrapper);
+        }
+    });
+});
+
+//add click event to each gif
+$(document).on("click", ".comic-image", function() {
+  //get current state of gif
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {  
+    //animate gif if state is still
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    //pause gif if state is animate
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
